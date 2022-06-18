8dd08d3e49c8c113e6d1fe9e9642c480b9dc57e0
Robert van Dijk <robert@citroenvlinder17.nl>
2022-06-18 19:59:09 +0200

    Add judgehost script

---
+++ start-judgehost.sh
@@ -0,0 +1,15 @@
+DOMSERVER_BASEURL=https://dj.chipcie.ch.tudelft.nl/
+JUDGEDAEMON_PASSWORD=jd_pass
+
+docker run \
+--rm \
+-d \
+--privileged \
+--cpuset-cpus="$1" \
+-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+-e DOMSERVER_BASEURL=$DOMSERVER_BASEURL \
+-e JUDGEDAEMON_PASSWORD=$JUDGEDAEMON_PASSWORD \
+--name judgehost-$1 \
+--hostname judgedaemon-$1 \
+-e DAEMON_ID=$1 \
+ghcr.io/wisvch/domjudge-packaging/judgehost:8.1.0
